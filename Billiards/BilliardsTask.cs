bae17ebf9b7dbd984393ce4f6f7f379381ce33c2
Anton <47969596+Sentine1@users.noreply.github.com>
2019-08-27 20:27:18 +0500

    some_lessons2-3seminar
    No sence, just lessons answer

---
+++ Billiards/BilliardsTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Billiards
+{
+    public static class BilliardsTask
+    {
+        //        /// <summary>
+        //        /// 
+        //        /// </summary>
+        //        /// <param name="directionRadians">Угол направелния движения шара</param>
+        //        /// <param name="wallInclinationRadians">Угол</param>
+        //        /// <returns></returns>
+        //        //public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        //        //{
+        //        //    //TODO
+        //        //    directionRadians=(directionRadians * 180) / Math.PI;
+        //        //    wallInclinationRadians = (wallInclinationRadians * 180) / Math.PI;
+        //        //    Double a= 90+wallInclinationRadians - 180+directionRadians,b=180+ directionRadians + 2 * a + (360 * 5);
+        //        //    b %= 360;
+        //        //    if (b > 180)
+        //        //    {
+        //        //        b -= 360;
+        //        //    }
+        //        //    return b*Math.PI/180;
+        //        //}
+        //    }
+        //}
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            double direction = (directionRadians * 180) / Math.PI;
+            double wallInclination = (wallInclinationRadians * 180) / Math.PI;
+            double wallAngle = 90 + wallInclination;
+            double ballAngle = 180 + direction;
+            double diff = wallAngle - ballAngle;
+            double angle = ballAngle + 2 * diff + 360 * 5;
+            angle %= 360;
+            if (angle > 180)
+            {
+                angle -= 360;
+            }
+            return (angle * Math.PI) / 180;
+        }
+    }
+}
--- Distance/DistanceTask.cs
+++ Distance/DistanceTask.cs
@@ -2,22 +2,22 @@
 
 namespace DistanceTask
 {
-	public static class DistanceTask
-	{
-        // Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
+    public static class DistanceTask
+    {
+        // Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by) 
         public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
         {
-            //exp
-            double xax = Math.Sqrt((ax-x)*(ax-x)+(ay-y)*(ay-y));
-            double bax = Math.Sqrt((bx - x) * (bx - x) + (by - y) * (by - y));
-            double ab = Math.Sqrt((ax-bx)*(ax-bx)+(ay-by)* (ay - by));
-            //формула ещё из Expr6. Посчитать расстояние от точки до прямой, заданной двумя разными точками.
-            double hieght = Math.Abs((by - ay) * x - (bx - ax) * y + (bx * ay - by * ax)) / Math.Sqrt((Math.Pow((by - ay), 2)) + (Math.Pow((bx - ax), 2)));
-            if ((ax == x || bx == x) && (ay == y || by == y)) return 0;
-             if ((x>Math.Min(ax,bx)&&x<Math.Max(ax,bx)&&x-hieght >= Math.Min(ax, bx) && x-hieght <= Math.Max(ax, bx) )|| (y>Math.Min(ay,by)&&y<Math.Max(ay,by)&& y-hieght >= Math.Min(ay, by) && y-hieght <= Math.Max(ay, by)))
-             return hieght;
-             else if (xax > bax) return bax;
-            else return xax;
+            double ab = (x-ax)*(bx-ax)+(y-ay)*(by-ay);
+            ab = (ab / ((bx - ax) * (bx - ax) + ((by - ay) * (by - ay))));
+            if (ab < 0 || double.IsNaN(ab))
+                ab = 0;
+            if (ab > 1)
+                ab = 1;
+            double xaxbx = (ax - x + ((bx - ax) * ab));
+            double yayby = (ay - y + ((by - ay) * ab));
+            double hieght =Math.Sqrt((xaxbx * xaxbx) + (yayby * yayby));
+            return hieght;
+            
         }
     }
 }
